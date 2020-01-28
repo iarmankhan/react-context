8b410e1a47c09a86fe492f616dcf213792d68819
iarmankhan <armankhan9244@gmail.com>
2020-01-28 15:29:16 +0530

    Multiple contexts using hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,39 +1,24 @@
-import React, {Component} from 'react';
+import React, {useContext} from 'react';
 import {ThemeContext} from '../contexts/ThemeContext'
 import { AuthContext } from '../contexts/AuthContext';
 
-class NavBar extends Component {
-
-    render() { 
-        
-        return (
-            <AuthContext.Consumer>{(authContext) => {
-                return (
-                    <ThemeContext.Consumer>{(themeContext) => {
-                        const {isAuthenticated, toggleAuth} =authContext;
-                        const { isLightTheme, light, dark } = themeContext;
-                        const theme = isLightTheme ? light : dark;
-                        return (
-                            <nav style={{background: theme.ui, color: theme.syntax}}>
-                                <h1>Context App</h1>
-                                <div onClick={toggleAuth}>
-                                    {isAuthenticated ? 'Logged In' : 'Logged Out'}
-                                </div>
-                                <ul>
-                                    <li>Home</li>
-                                    <li>About</li>
-                                    <li>Contact</li>
-                                </ul>
-                            </nav>
-                        )
-                    }}
-                    </ThemeContext.Consumer>
-                )
-            }}
-            
-            </AuthContext.Consumer>
-        );
-    }
+const NavBar = () => {
+    const {isLightTheme, light, dark} = useContext(ThemeContext)
+    const theme = isLightTheme ? light : dark;
+    const {isAuthenticated, toggleAuth} = useContext(AuthContext);
+    return (
+        <nav style={{background: theme.ui, color: theme.syntax}}>
+            <h1>Context App</h1>
+            <div onClick={toggleAuth}>
+                {isAuthenticated ? 'Logged In' : 'Logged Out'}
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
 }
  
 export default NavBar;
